6f8950f93e4ee42926fc60c3c1ec3c9add2b020b
Mustago <muga200301@gmail.com>
2024-03-03 12:11:40 +0500

    burger add

--- app/ui/nabar.tsx
+++ app/ui/nabar.tsx
@@ -1,36 +1,53 @@
-'use client'
-import Link from "next/link";
-import Image from "next/image";
-import todo from '@/public/todo.png'
-import {usePathname} from "next/navigation";
-import clsx from "clsx";
+'use client';
+import Link from 'next/link';
+import Image from 'next/image';
+import todo from '@/public/todo.png';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+import { FcMenu } from 'react-icons/fc';
+import { useState } from 'react';
 
 const links = [
-    { name: 'Cписок задач', href: '/todo', icon: todo, alt:'icon Todo' },
+  { name: 'Cписок задач', href: '/todo', icon: todo, alt: 'icon Todo' },
 
-    { name: 'Управление финансами', href: '/finance', icon:todo, alt: 'icon Price' },
+  {
+    name: 'Управление финансами',
+    href: '/finance',
+    icon: todo,
+    alt: 'icon Price',
+  },
 ];
-export default function Navbar(){
-    const path = usePathname()
-
-    return (
-        <div>
-            <ul className='flex items-center gap-[20px]'>
-
-
-            {
-                links.map((el, id) => {
-                    return (
-                        <li key={id} className='flex gap-[10px] items-center'>
-                            <Image src={el.icon} alt={el.alt} width={40} height={40}  />
-                            <Link href={el.href} className={clsx('text-center', {
-                                'text-blue-600' : path == el.href
-                            })}>{el.name}</Link>
-                        </li>
-                    )
-                })
-            }
-            </ul>
-        </div>
-    )
+export default function Navbar() {
+  const path = usePathname();
+const [hidden, setHidden] = useState<boolean>(true);
+  return (
+    <div className="flex items-center sm:justify-center justify-end relative">
+      <div className="sm:hidden ">
+        <button onClick={() => setHidden((prev) => !prev)}>
+          <FcMenu size={40} />
+        </button>
+      </div>
+      <ul className={clsx(" items-center gap-[20px] sm:flex", {
+        'flex flex-col w-48 h-14 items-start absolute top-11': hidden==true,
+        'hidden' : hidden ==false
+      })}>
+        {links.map((el, id) => {
+          return (
+            <li key={id} className="flex items-center gap-[10px]">
+              <Image src={el.icon} alt={el.alt} width={40} height={40} />
+              <Link
+              onClick={() => setHidden(false)}
+                href={el.href}
+                className={clsx('text-center', {
+                  'text-blue-600': path == el.href,
+                })}
+              >
+                {el.name}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
--- app/ui/sideNav.tsx
+++ app/ui/sideNav.tsx
@@ -3,12 +3,12 @@ import todo from '@/public/todo.png';
 import Navbar from '@/app/ui/nabar';
 export default function SideNav() {
   return (
-    <div className="grid grid-cols-3 items-center">
-      <div className="">
+    <div className="flex justify-around items-center">
+      <div className="flex items-center gap-[15px] sm:block">
         <Image src={todo} alt="todo" width={100} height={100} />
         <h1>TODO TJ</h1>
       </div>
-      <div className="col-span-2">
+      <div className="">
         <Navbar />
       </div>
     </div>
