3172eec485aefbce54d64b539204e385c8bca649
Mustago <muga200301@gmail.com>
2024-03-16 11:10:29 +0500

    fixed bag

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -2,16 +2,14 @@ import { sql } from '@vercel/postgres';
 import { TodoType } from './definition';
 import { unstable_noStore as noStore } from 'next/cache';
 import { cookies } from 'next/headers';
-import { jwtDecode } from 'jwt-decode';
+
 export async function fetchUser() {
   return 'null';
 }
 export async function fetchTodo() {
   noStore();
-  const currentUser = cookies().get('currentUser')?.value;
-  const userData: [a: string, b: string] = jwtDecode(String(currentUser));
   const data = await sql<TodoType>`
-    SELECT * FROM todos WHERE user_id = ${userData[0]}`;
+    SELECT * FROM todos WHERE user_id = "ffdfds"`;
   return data.rows;
 }
 
