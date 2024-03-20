9ced14e9a143c9114e98cef6bda91bed29324383
Mustago <muga200301@gmail.com>
2024-03-20 11:12:14 +0500

    added matrika v1.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,13 @@ import { Metadata } from 'next';
 import {inter} from '@/app/ui/font'
 import SideNav from './ui/sideNav';
 import '@/app/ui/global.css'
+import { Suspense } from 'react';
+import { Metrika } from '@/app/components/mertika';
 export const metadata: Metadata = {
   title: 'Todo TJ',
   description: 'The official Next.js Course Dashboard, built with App Router.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+
 };
 export default function RootLayout({
   children,
@@ -23,21 +26,9 @@ export default function RootLayout({
 
         <main>{children}</main>
         <section id="modal"></section>
-  
-<Script type="text/javascript" strategy="afterInteractive">
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();
-   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-   ym(96793920, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true
-   });
-</Script>
-<noscript><div><img src="https://mc.yandex.ru/watch/96793920" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+          <Suspense>
+          <Metrika />
+        </Suspense>
       </body>
     </html>
   );
